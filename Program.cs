a6691c75abf77591ac8e1a9ddc69bcf301d1055c
N4choCM <104795448+N4choCM@users.noreply.github.com>
2022-07-09 15:45:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Ejercicio 1: Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Por favor, introduzca su nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, ¡{0}!", nombre);
+
+//Ejercicio 2: Escribe un programa que tome la hora y la escriba por pantalla.
+Console.WriteLine("¿Qué hora es?");
+string hora = Console.ReadLine();
+Console.WriteLine("Son las {0}.", hora);
